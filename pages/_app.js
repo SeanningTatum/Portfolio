500a18cad900a759b8310cc67e5e8fb42d264378
SeanningTatum <seantheurgel@gmail.com>
2021-03-26 15:34:05 +0800

    update description

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }) {
         <title>Sean Urgel</title>
         <meta
           name="Description"
-          content="A Next.js starter styled using Tailwind CSS."
+          content="Software Engineer, Mobile Developer, Fullstack Developer and Food lover"
         />
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👨‍💻</text></svg>" />
 
